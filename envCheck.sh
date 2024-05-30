497fb15fe099df79582012a83e6f5009a728f208
rymnc <43716372+rymnc@users.noreply.github.com>
2024-05-30 14:07:47 +0530

    fix: envCheck

--- envCheck.sh
+++ envCheck.sh
@@ -1,7 +1,10 @@
 # This script is used to assert if require env vars are present for deployment
 # RPC_URL: RPC URL for the network
 # ACCOUNT: Accessed with `cast wallet` command
-# ETH_FROM: Address to send transactions from TODO: remove this after bug in foundry is fixed
+# ETH_FROM: Address to send transactions from
+# we need ETH_FROM because of the following bug:
+# https://github.com/foundry-rs/foundry/issues/7255
+
 
 if [ -z "$RPC_URL" ]; then
   echo "RPC_URL is required"
